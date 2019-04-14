c02a0ad94b709708baea0473d1b7fa34f9333e2a
David <regev.david06@gmail.com>
2019-04-14 17:59:09 +0300

    Tcp Server

---
+++ FlightSimulator/Model/TCPServer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class TCPServer
+    {
+        private string IP;
+        private int port;
+        private TcpListener listener;
+        private ClientHandler clientHandler;
+        
+        public TCPServer(string IP, int port)
+        {
+            this.IP = IP;
+            this.port = port;
+        }
+
+        public void start()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP), port);
+            TcpListener server = new TcpListener(ep);
+
+            try
+            {
+                server.Start();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+                        Console.WriteLine("Got new connection");
+                        clientHandler.HandleClient(client);
+                    }
+                    catch (SocketException)
+                    {
+                        break;
+                    }
+
+                }
+            });
+            t.Start();
+            
+        }
+
+
+
+        
+    }
+}
