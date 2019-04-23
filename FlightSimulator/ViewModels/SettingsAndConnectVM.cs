7640cc511af798488fae7aa38ee0f8a554556223
David <regev.david06@gmail.com>
2019-04-23 19:38:38 +0300

    Test Final

--- FlightSimulator/ViewModels/SettingsAndConnectVM.cs
+++ FlightSimulator/ViewModels/SettingsAndConnectVM.cs
@@ -69,6 +69,7 @@ namespace FlightSimulator.Views
             TCPClient client = new TCPClient();
             client.Close();
             server.Stop();
+            is_connect = false;
         }
 
     }
